b0b7b955bf9b0583d9bedf1d40c4c949631fe8b4
JohnGuilding <jwguilding@gmail.com>
2024-04-24 16:02:41 +0100

    Stub integration test

---
+++ src/test/OwnableValidator.sol
@@ -0,0 +1,117 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.23;
+
+import {ERC7579ValidatorBase} from "modulekit/Modules.sol";
+import {PackedUserOperation} from "modulekit/external/ERC4337.sol";
+
+import {SignatureCheckerLib} from "solady/src/utils/SignatureCheckerLib.sol";
+import {ECDSA} from "solady/src/utils/ECDSA.sol";
+
+contract OwnableValidator is ERC7579ValidatorBase {
+    using SignatureCheckerLib for address;
+
+    /*//////////////////////////////////////////////////////////////////////////
+                            CONSTANTS & STORAGE
+    //////////////////////////////////////////////////////////////////////////*/
+
+    error NotAuthorized();
+
+    mapping(address subAccout => address owner) public owners;
+
+    /** account to authorized account to authorization */
+    mapping(address => mapping(address => bool)) public authorized;
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                     CONFIG
+    //////////////////////////////////////////////////////////////////////////*/
+
+    function onInstall(bytes calldata data) external override {
+        if (data.length == 0) return;
+        (address owner, address authorizedAccount) = abi.decode(
+            data,
+            (address, address)
+        );
+        owners[msg.sender] = owner;
+        authorized[msg.sender][authorizedAccount] = true;
+    }
+
+    // FIXME: attacker could overcome authorized timelock by uninstalling and installing the module again
+    function onUninstall(bytes calldata) external override {
+        delete owners[msg.sender];
+        // delete authorized[msg.sender][authorizedAccount];
+    }
+
+    function isInitialized(address smartAccount) external view returns (bool) {
+        return owners[smartAccount] != address(0);
+    }
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                     MODULE LOGIC
+    //////////////////////////////////////////////////////////////////////////*/
+
+    function validateUserOp(
+        PackedUserOperation calldata userOp,
+        bytes32 userOpHash
+    ) external view override returns (ValidationData) {
+        bool validSig = owners[userOp.sender].isValidSignatureNow(
+            ECDSA.toEthSignedMessageHash(userOpHash),
+            userOp.signature
+        );
+        return _packValidationData(!validSig, type(uint48).max, 0);
+    }
+
+    function isValidSignatureWithSender(
+        address,
+        bytes32 hash,
+        bytes calldata data
+    ) external view override returns (bytes4) {
+        address owner = owners[msg.sender];
+        return
+            SignatureCheckerLib.isValidSignatureNowCalldata(owner, hash, data)
+                ? EIP1271_SUCCESS
+                : EIP1271_FAILED;
+    }
+
+    function changeOwner(
+        address account,
+        address newOwner
+    ) external onlyAuthorized(account) {
+        owners[account] = newOwner;
+    }
+
+    /**
+     * @notice Adds special permissions for an account to execute priviliged actions
+     * on the module, notably changing the owner. This is useful for adding a recovery module.
+     * @dev in order for this function to remain secure, the delay time must be longer than
+     * the time it takes to authorize an action from the calling account. For example, if there
+     * is a recovery delay of 1 day on a recovery module to protect against malicious guardians,
+     * the delay for this function must be longer than that time so that an attacker could not
+     * authorize itself before the recovery attempt succeeds.
+     */
+    function authorizeAccount(address accountToAuthorize) public {
+        authorized[msg.sender][accountToAuthorize] = true;
+    }
+
+    modifier onlyAuthorized(address account) {
+        if (authorized[account][msg.sender] == false) revert NotAuthorized();
+        _;
+    }
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                     METADATA
+    //////////////////////////////////////////////////////////////////////////*/
+
+    function name() external pure returns (string memory) {
+        return "OwnableValidator";
+    }
+
+    function version() external pure returns (string memory) {
+        return "0.0.1";
+    }
+
+    function isModuleType(
+        uint256 typeID
+    ) external pure override returns (bool) {
+        return typeID == TYPE_VALIDATOR;
+    }
+}
--- test/ZkEmailRecovery.t.sol
+++ test/ZkEmailRecovery.t.sol
@@ -3,8 +3,15 @@ pragma solidity ^0.8.23;
 
 import {Test} from "forge-std/Test.sol";
 import {RhinestoneModuleKit, ModuleKitHelpers, ModuleKitUserOp, AccountInstance, UserOpData} from "modulekit/ModuleKit.sol";
-import {MODULE_TYPE_EXECUTOR} from "modulekit/external/ERC7579.sol";
+import {MODULE_TYPE_EXECUTOR, MODULE_TYPE_VALIDATOR} from "modulekit/external/ERC7579.sol";
+import {ECDSA} from "solady/src/utils/ECDSA.sol";
+
 import {ZkEmailRecovery} from "src/ZkEmailRecovery.sol";
+import {OwnableValidator} from "src/test/OwnableValidator.sol";
+
+import {EmailAuth, EmailAuthMsg, EmailProof} from "ether-email-auth/packages/contracts/src/EmailAuth.sol";
+import {ECDSAOwnedDKIMRegistry} from "ether-email-auth/packages/contracts/src/utils/ECDSAOwnedDKIMRegistry.sol";
+import {MockGroth16Verifier} from "../src/test/MockGroth16Verifier.sol";
 
 contract ZkEmailRecoveryTest is RhinestoneModuleKit, Test {
     using ModuleKitHelpers for *;
@@ -13,47 +20,114 @@ contract ZkEmailRecoveryTest is RhinestoneModuleKit, Test {
     // account and modules
     AccountInstance internal instance;
     ZkEmailRecovery internal executor;
+    OwnableValidator internal validator;
+
+    address public owner;
+
+    // ZK Email contracts and variables
+    address zkEmailDeployer = vm.addr(1);
+    ECDSAOwnedDKIMRegistry ecdsaOwnedDkimRegistry;
+    MockGroth16Verifier verifier;
+    bytes32 accountSalt1;
+    bytes32 accountSalt2;
+
+    address guardian1;
+    address guardian2;
+
+    string selector = "12345";
+    string domainName = "gmail.com";
+    bytes32 publicKeyHash =
+        0x0ea9c777dc7110e5a9e89b13f0cfc540e3845ba120b2b6dc24024d61488d4788;
 
     function setUp() public {
         init();
 
+        // Create ZK Email contracts
+        vm.startPrank(zkEmailDeployer);
+        ecdsaOwnedDkimRegistry = new ECDSAOwnedDKIMRegistry(zkEmailDeployer);
+        string memory signedMsg = ecdsaOwnedDkimRegistry.computeSignedMsg(
+            ecdsaOwnedDkimRegistry.SET_PREFIX(),
+            selector,
+            domainName,
+            publicKeyHash
+        );
+        bytes32 digest = ECDSA.toEthSignedMessageHash(bytes(signedMsg));
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(1, digest);
+        bytes memory signature = abi.encodePacked(r, s, v);
+        ecdsaOwnedDkimRegistry.setDKIMPublicKeyHash(
+            selector,
+            domainName,
+            publicKeyHash,
+            signature
+        );
+
+        verifier = new MockGroth16Verifier();
+        accountSalt1 = keccak256(abi.encode("account salt 1"));
+        accountSalt2 = keccak256(abi.encode("account salt 2"));
+
+        EmailAuth emailAuthImpl = new EmailAuth();
+        vm.stopPrank();
+
+        address[] memory owners = new address[](1);
+        owner = vm.createWallet("Alice").addr;
+        owners[0] = owner;
+
         // Create the executor
-        executor = new ZkEmailRecovery(address(0), address(0), address(0));
+        executor = new ZkEmailRecovery(
+            address(verifier),
+            address(ecdsaOwnedDkimRegistry),
+            address(emailAuthImpl)
+        );
         vm.label(address(executor), "ZkEmailRecovery");
+        validator = new OwnableValidator();
 
         // Create the account and install the executor
         instance = makeAccountInstance("ZkEmailRecovery");
         vm.deal(address(instance.account), 10 ether);
+
+        guardian1 = executor.computeEmailAuthAddress(accountSalt1);
+        guardian2 = executor.computeEmailAuthAddress(accountSalt2);
+
+        address[] memory guardians;
+        guardians[0] = guardian1;
+        guardians[1] = guardian2;
+        uint256 recoveryDelay = 1 seconds;
+        uint256 threshold = 2;
+
         instance.installModule({
             moduleTypeId: MODULE_TYPE_EXECUTOR,
             module: address(executor),
-            data: ""
+            data: abi.encode(guardians, recoveryDelay, threshold)
+        });
+
+        instance.installModule({
+            moduleTypeId: MODULE_TYPE_VALIDATOR,
+            module: address(validator),
+            data: abi.encode(owner, owner)
         });
     }
 
-    function testExec() public {
-        // Create a target address and send some ether to it
-        address target = makeAddr("target");
-        uint256 value = 1 ether;
-
-        // Get the current balance of the target
-        uint256 prevBalance = target.balance;
-
-        // Execute the call
-        // EntryPoint -> Account -> Executor -> Account -> Swap
-        UserOpData memory userOpData = instance.getExecOps({
-            target: address(executor),
-            value: 0,
-            callData: abi.encodeWithSelector(
-                ZkEmailRecovery.completeRecovery.selector
-            ),
-            txValidator: address(instance.defaultValidator)
-        });
-
-        // Execute the userOp
-        userOpData.execUserOps();
-
-        // Check if the balance of the target has increased
-        assertEq(target.balance, prevBalance + value);
+    function testRecover() public {
+        // Functions we need to call without going through a validator and instead would be called via a relayer
+        //
+        // 1.
+        // IZkEmailRecovery(routerAddress).handleAcceptance(
+        //     emailAuthMsg,
+        //     templateIdx
+        // );
+        //
+        // 2.
+        // IZkEmailRecovery(routerAddress).handleRecovery(
+        //     emailAuthMsg,
+        //     templateIdx
+        // );
+        //
+        // 3.
+        // IZkEmailRecovery(routerAddress).completeRecovery();
+        //
+        // Assert owner has changed
+        //
+        // assertTrue(isOwner);
+        // assertFalse(oldOwnerIsOwner);
     }
 }
