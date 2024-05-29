5188356e20086c80c81de7b691d6b2f5adda80d5
JohnGuilding <jwguilding@gmail.com>
2024-05-29 18:59:56 +0100

    Add cancelRecovery & changeThreshold tests

--- test/unit/ZkEmailRecovery/cancelRecovery.t.sol
+++ test/unit/ZkEmailRecovery/cancelRecovery.t.sol
@@ -3,11 +3,134 @@ pragma solidity ^0.8.25;
 
 import "forge-std/console2.sol";
 import {UnitBase} from "../UnitBase.t.sol";
+import {IZkEmailRecovery} from "src/interfaces/IZkEmailRecovery.sol";
+import {OwnableValidatorRecoveryModule} from "src/modules/OwnableValidatorRecoveryModule.sol";
 
 contract ZkEmailRecovery_cancelRecovery_Test is UnitBase {
+    OwnableValidatorRecoveryModule recoveryModule;
+    address recoveryModuleAddress;
+
     function setUp() public override {
         super.setUp();
+
+        recoveryModule = new OwnableValidatorRecoveryModule{salt: "test salt"}(
+            address(zkEmailRecovery)
+        );
+        recoveryModuleAddress = address(recoveryModule);
     }
 
-    function test_CancelRecovery_Succeeds() public {}
+    function test_CancelRecovery_CannotCancelWrongRecoveryRequest() public {
+        address otherAddress = address(99);
+
+        vm.startPrank(accountAddress);
+        zkEmailRecovery.configureRecovery(
+            recoveryModuleAddress,
+            guardians,
+            guardianWeights,
+            threshold,
+            delay,
+            expiry
+        );
+        vm.stopPrank();
+
+        acceptGuardian(accountSalt1);
+        vm.warp(12 seconds);
+        handleRecovery(recoveryModuleAddress, accountSalt1);
+
+        IZkEmailRecovery.RecoveryRequest
+            memory recoveryRequest = zkEmailRecovery.getRecoveryRequest(
+                accountAddress
+            );
+        assertEq(recoveryRequest.executeAfter, 0);
+        assertEq(recoveryRequest.executeBefore, 0);
+        assertEq(recoveryRequest.currentWeight, 1);
+        assertEq(recoveryRequest.subjectParams.length, 0);
+
+        vm.startPrank(otherAddress);
+        zkEmailRecovery.cancelRecovery("");
+
+        recoveryRequest = zkEmailRecovery.getRecoveryRequest(accountAddress);
+        assertEq(recoveryRequest.executeAfter, 0);
+        assertEq(recoveryRequest.executeBefore, 0);
+        assertEq(recoveryRequest.currentWeight, 1);
+        assertEq(recoveryRequest.subjectParams.length, 0);
+    }
+
+    function test_CancelRecovery_PartialRequest_Succeeds() public {
+        vm.startPrank(accountAddress);
+        zkEmailRecovery.configureRecovery(
+            recoveryModuleAddress,
+            guardians,
+            guardianWeights,
+            threshold,
+            delay,
+            expiry
+        );
+        vm.stopPrank();
+
+        acceptGuardian(accountSalt1);
+        vm.warp(12 seconds);
+        handleRecovery(recoveryModuleAddress, accountSalt1);
+
+        IZkEmailRecovery.RecoveryRequest
+            memory recoveryRequest = zkEmailRecovery.getRecoveryRequest(
+                accountAddress
+            );
+        assertEq(recoveryRequest.executeAfter, 0);
+        assertEq(recoveryRequest.executeBefore, 0);
+        assertEq(recoveryRequest.currentWeight, 1);
+        assertEq(recoveryRequest.subjectParams.length, 0);
+
+        vm.startPrank(accountAddress);
+        zkEmailRecovery.cancelRecovery("");
+
+        recoveryRequest = zkEmailRecovery.getRecoveryRequest(accountAddress);
+        assertEq(recoveryRequest.executeAfter, 0);
+        assertEq(recoveryRequest.executeBefore, 0);
+        assertEq(recoveryRequest.currentWeight, 0);
+        assertEq(recoveryRequest.subjectParams.length, 0);
+    }
+
+    function test_CancelRecovery_FullRequest_Succeeds() public {
+        vm.startPrank(accountAddress);
+        zkEmailRecovery.configureRecovery(
+            recoveryModuleAddress,
+            guardians,
+            guardianWeights,
+            threshold,
+            delay,
+            expiry
+        );
+        vm.stopPrank();
+
+        acceptGuardian(accountSalt1);
+        acceptGuardian(accountSalt2);
+        vm.warp(12 seconds);
+        handleRecovery(recoveryModuleAddress, accountSalt1);
+        handleRecovery(recoveryModuleAddress, accountSalt2);
+
+        IZkEmailRecovery.RecoveryRequest
+            memory recoveryRequest = zkEmailRecovery.getRecoveryRequest(
+                accountAddress
+            );
+        assertEq(recoveryRequest.executeAfter, block.timestamp + delay);
+        assertEq(recoveryRequest.executeBefore, block.timestamp + expiry);
+        assertEq(recoveryRequest.currentWeight, 3);
+        assertEq(recoveryRequest.subjectParams.length, 3);
+        assertEq(recoveryRequest.subjectParams[0], abi.encode(accountAddress));
+        assertEq(recoveryRequest.subjectParams[1], abi.encode(newOwner));
+        assertEq(
+            recoveryRequest.subjectParams[2],
+            abi.encode(recoveryModuleAddress)
+        );
+
+        vm.startPrank(accountAddress);
+        zkEmailRecovery.cancelRecovery("");
+
+        recoveryRequest = zkEmailRecovery.getRecoveryRequest(accountAddress);
+        assertEq(recoveryRequest.executeAfter, 0);
+        assertEq(recoveryRequest.executeBefore, 0);
+        assertEq(recoveryRequest.currentWeight, 0);
+        assertEq(recoveryRequest.subjectParams.length, 0);
+    }
 }
--- test/unit/ZkEmailRecovery/changeThreshold.t.sol
+++ test/unit/ZkEmailRecovery/changeThreshold.t.sol
@@ -3,15 +3,132 @@ pragma solidity ^0.8.25;
 
 import "forge-std/console2.sol";
 import {UnitBase} from "../UnitBase.t.sol";
+import {IZkEmailRecovery} from "src/interfaces/IZkEmailRecovery.sol";
+import {OwnableValidatorRecoveryModule} from "src/modules/OwnableValidatorRecoveryModule.sol";
 
 contract ZkEmailRecovery_changeThreshold_Test is UnitBase {
+    OwnableValidatorRecoveryModule recoveryModule;
+    address recoveryModuleAddress;
+
     function setUp() public override {
         super.setUp();
+
+        recoveryModule = new OwnableValidatorRecoveryModule{salt: "test salt"}(
+            address(zkEmailRecovery)
+        );
+        recoveryModuleAddress = address(recoveryModule);
     }
 
-    function test_RevertWhen_MsgSenderNotConfigured() public {}
-    function test_RevertWhen_AlreadyRecovering() public {}
-    function test_RevertWhen_ThresholdExceedsGuardianCount() public {}
-    function test_RevertWhen_ThresholdIsZero() public {}
-    function test_ChangeThreshold_Succeeds() public {}
+    function test_RevertWhen_AlreadyRecovering() public {
+        vm.startPrank(accountAddress);
+        zkEmailRecovery.configureRecovery(
+            recoveryModuleAddress,
+            guardians,
+            guardianWeights,
+            threshold,
+            delay,
+            expiry
+        );
+        vm.stopPrank();
+
+        acceptGuardian(accountSalt1);
+        vm.warp(12 seconds);
+        handleRecovery(recoveryModuleAddress, accountSalt1);
+
+        vm.startPrank(accountAddress);
+        vm.expectRevert(IZkEmailRecovery.RecoveryInProcess.selector);
+        zkEmailRecovery.changeThreshold(threshold);
+    }
+
+    function test_RevertWhen_SetupNotCalled() public {
+        vm.expectRevert(IZkEmailRecovery.SetupNotCalled.selector);
+        zkEmailRecovery.changeThreshold(threshold);
+    }
+
+    function test_RevertWhen_ThresholdExceedsTotalWeight() public {
+        uint256 highThreshold = totalWeight + 1;
+
+        vm.startPrank(accountAddress);
+        zkEmailRecovery.configureRecovery(
+            recoveryModuleAddress,
+            guardians,
+            guardianWeights,
+            threshold,
+            delay,
+            expiry
+        );
+        vm.stopPrank();
+
+        vm.startPrank(accountAddress);
+        vm.expectRevert(
+            IZkEmailRecovery.ThresholdCannotExceedTotalWeight.selector
+        );
+        zkEmailRecovery.changeThreshold(highThreshold);
+    }
+    function test_RevertWhen_ThresholdIsZero() public {
+        uint256 zeroThreshold = 0;
+
+        vm.startPrank(accountAddress);
+        zkEmailRecovery.configureRecovery(
+            recoveryModuleAddress,
+            guardians,
+            guardianWeights,
+            threshold,
+            delay,
+            expiry
+        );
+        vm.stopPrank();
+
+        vm.startPrank(accountAddress);
+        vm.expectRevert(IZkEmailRecovery.ThresholdCannotBeZero.selector);
+        zkEmailRecovery.changeThreshold(zeroThreshold);
+    }
+
+    function test_ChangeThreshold_IncreaseThreshold() public {
+        uint256 newThreshold = threshold + 1;
+        vm.startPrank(accountAddress);
+        zkEmailRecovery.configureRecovery(
+            recoveryModuleAddress,
+            guardians,
+            guardianWeights,
+            threshold,
+            delay,
+            expiry
+        );
+        vm.stopPrank();
+
+        vm.startPrank(accountAddress);
+        vm.expectEmit();
+        emit IZkEmailRecovery.ChangedThreshold(newThreshold);
+        zkEmailRecovery.changeThreshold(newThreshold);
+
+        IZkEmailRecovery.GuardianConfig memory guardianConfig = zkEmailRecovery
+            .getGuardianConfig(accountAddress);
+        assertEq(guardianConfig.guardianCount, guardians.length);
+        assertEq(guardianConfig.threshold, newThreshold);
+    }
+
+    function test_ChangeThreshold_DecreaseThreshold() public {
+        uint256 newThreshold = threshold - 1;
+        vm.startPrank(accountAddress);
+        zkEmailRecovery.configureRecovery(
+            recoveryModuleAddress,
+            guardians,
+            guardianWeights,
+            threshold,
+            delay,
+            expiry
+        );
+        vm.stopPrank();
+
+        vm.startPrank(accountAddress);
+        vm.expectEmit();
+        emit IZkEmailRecovery.ChangedThreshold(newThreshold);
+        zkEmailRecovery.changeThreshold(newThreshold);
+
+        IZkEmailRecovery.GuardianConfig memory guardianConfig = zkEmailRecovery
+            .getGuardianConfig(accountAddress);
+        assertEq(guardianConfig.guardianCount, guardians.length);
+        assertEq(guardianConfig.threshold, newThreshold);
+    }
 }
