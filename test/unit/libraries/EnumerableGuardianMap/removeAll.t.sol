c9d60c9638d963e40b695bcce6fb9ca028ced1d4
wshino <wshinohara@gmail.com>
2024-06-21 20:28:34 +0900

    Add test cases for EnumerableGuardianMap.

--- test/unit/libraries/EnumerableGuardianMap/removeAll.t.sol
+++ test/unit/libraries/EnumerableGuardianMap/removeAll.t.sol
@@ -2,21 +2,82 @@
 pragma solidity ^0.8.25;
 
 import "forge-std/console2.sol";
-import { UnitBase } from "../../UnitBase.t.sol";
+import {UnitBase} from "../../UnitBase.t.sol";
+import {EnumerableGuardianMap, GuardianStorage, GuardianStatus} from "../../../../src/libraries/EnumerableGuardianMap.sol";
 
 contract EnumerableGuardianMap_removeAll_Test is UnitBase {
+    using EnumerableGuardianMap for EnumerableGuardianMap.AddressToGuardianMap;
+    mapping(address account => EnumerableGuardianMap.AddressToGuardianMap guardian)
+        internal guardiansStorage;
+
     function setUp() public override {
         super.setUp();
     }
 
-    function test_RemoveAll_RevertWhen_TooManyValuesToRemove() public view {
-        // TODO: test
+    function test_RemoveAll_Succeeds() public {
+        bool result;
+
+        for (uint256 i = 1; i <= 3; i++) {
+            result = guardiansStorage[accountAddress].set({
+                key: vm.addr(i),
+                value: GuardianStorage(
+                    GuardianStatus.REQUESTED,
+                    guardianWeights[1]
+                )
+            });
+            assertEq(result, true);
+        }
+        address[] memory addresses = new address[](3);
+        addresses[0] = vm.addr(1);
+        addresses[1] = vm.addr(2);
+        addresses[2] = vm.addr(3);
+        guardiansStorage[accountAddress].removeAll(addresses);
     }
 
-    function test_RemoveAll_Succeeds() public view {
-        // TODO: test
+    function test_RemoveAll_RemovesMaxNumberOfValues() public {
+        bool result;
+
+        for (
+            uint256 i = 1;
+            i <= EnumerableGuardianMap.MAX_NUMBER_OF_GUARDIANS;
+            i++
+        ) {
+            result = guardiansStorage[accountAddress].set({
+                key: vm.addr(i),
+                value: GuardianStorage(
+                    GuardianStatus.REQUESTED,
+                    guardianWeights[1]
+                )
+            });
+            assertEq(result, true);
+        }
+
+        address[] memory addresses = new address[](
+            EnumerableGuardianMap.MAX_NUMBER_OF_GUARDIANS
+        );
+        for (
+            uint256 i = 0;
+            i < EnumerableGuardianMap.MAX_NUMBER_OF_GUARDIANS;
+            i++
+        ) {
+            addresses[i] = vm.addr(i + 1);
+        }
+        guardiansStorage[accountAddress].removeAll(addresses);
     }
-    function test_RemoveAll_RemovesMaxNumberOfValues() public view {
-        // TODO: test
+
+    function test_RemoveAll_RevertWhen_TooManyValuesToRemove() public {
+        address[] memory addresses = new address[](
+            EnumerableGuardianMap.MAX_NUMBER_OF_GUARDIANS + 1
+        );
+
+        for (
+            uint256 i = 0;
+            i < EnumerableGuardianMap.MAX_NUMBER_OF_GUARDIANS + 1;
+            i++
+        ) {
+            addresses[i] = vm.addr(i + 1);
+        }
+        vm.expectRevert(EnumerableGuardianMap.TooManyValuesToRemove.selector);
+        guardiansStorage[accountAddress].removeAll(addresses);
     }
 }
--- test/unit/libraries/EnumerableGuardianMap/set.t.sol
+++ test/unit/libraries/EnumerableGuardianMap/set.t.sol
@@ -2,16 +2,109 @@
 pragma solidity ^0.8.25;
 
 import "forge-std/console2.sol";
-import { UnitBase } from "../../UnitBase.t.sol";
+import {UnitBase} from "../../UnitBase.t.sol";
+import {EnumerableGuardianMap, GuardianStorage, GuardianStatus} from "../../../../src/libraries/EnumerableGuardianMap.sol";
 
 contract EnumerableGuardianMap_set_Test is UnitBase {
+    using EnumerableGuardianMap for EnumerableGuardianMap.AddressToGuardianMap;
+    mapping(address account => EnumerableGuardianMap.AddressToGuardianMap guardian)
+        internal guardiansStorage;
+
     function setUp() public override {
         super.setUp();
     }
 
-    function test_Set_ReturnsFalseWhen_AddingKeysAlreadyInTheSet() public view { }
-    function test_Set_UpdatesValuesForKeysAlreadyInTheSet() public view { }
-    function test_Set_RevertWhen_MaxNumberOfGuardiansReached() public view { }
-    function test_Set_AddsAKey() public view { }
-    function test_Set_AddsSeveralKeys() public view { }
+    function test_Set_AddsAKey() public {
+        guardiansStorage[accountAddress].set({
+            key: guardian1,
+            value: GuardianStorage(GuardianStatus.REQUESTED, guardianWeights[1])
+        });
+    }
+
+    function test_Set_AddsSeveralKeys() public {
+        bool result;
+
+        result = guardiansStorage[vm.addr(1)].set({
+            key: guardian1,
+            value: GuardianStorage(GuardianStatus.REQUESTED, guardianWeights[1])
+        });
+        assertEq(result, true);
+        result = guardiansStorage[vm.addr(2)].set({
+            key: guardian2,
+            value: GuardianStorage(GuardianStatus.REQUESTED, guardianWeights[2])
+        });
+        assertEq(result, true);
+        require(
+            guardiansStorage[vm.addr(1)]._values[guardian1].status ==
+                GuardianStatus.REQUESTED,
+            "Expected status to be REQUESTED"
+        );
+        require(
+            guardiansStorage[vm.addr(2)]._values[guardian2].status ==
+                GuardianStatus.REQUESTED,
+            "Expected status to be REQUESTED"
+        );
+        require(
+            guardiansStorage[vm.addr(3)]._values[guardian3].status ==
+                GuardianStatus.NONE,
+            "Expected status to be NONE"
+        );
+    }
+
+    function test_Set_ReturnsFalseWhen_AddingKeysAlreadyInTheSet() public {
+        bool result;
+
+        result = guardiansStorage[vm.addr(1)].set({
+            key: guardian1,
+            value: GuardianStorage(GuardianStatus.REQUESTED, guardianWeights[1])
+        });
+        assertEq(result, true);
+        result = guardiansStorage[vm.addr(1)].set({
+            key: guardian1,
+            value: GuardianStorage(GuardianStatus.REQUESTED, guardianWeights[1])
+        });
+        assertEq(result, false);
+    }
+
+    function test_Set_UpdatesValuesForKeysAlreadyInTheSet() public {
+        bool result;
+
+        result = guardiansStorage[vm.addr(1)].set({
+            key: guardian1,
+            value: GuardianStorage(GuardianStatus.REQUESTED, guardianWeights[1])
+        });
+        assertEq(result, true);
+        result = guardiansStorage[vm.addr(1)].set({
+            key: guardian1,
+            value: GuardianStorage(GuardianStatus.ACCEPTED, guardianWeights[1])
+        });
+        assertEq(result, false);
+        require(
+            guardiansStorage[vm.addr(1)]._values[guardian1].status ==
+                GuardianStatus.ACCEPTED,
+            "Expected status to be ACCEPTED"
+        );
+    }
+
+    function test_Set_RevertWhen_MaxNumberOfGuardiansReached() public {
+        bool result;
+        // TODO: Can it be acceptable to number 33?
+        for (uint256 i = 1; i <= 33; i++) {
+            result = guardiansStorage[accountAddress].set({
+                key: vm.addr(i),
+                value: GuardianStorage(
+                    GuardianStatus.REQUESTED,
+                    guardianWeights[1]
+                )
+            });
+            assertEq(result, true);
+        }
+        vm.expectRevert(
+            EnumerableGuardianMap.MaxNumberOfGuardiansReached.selector
+        );
+        guardiansStorage[accountAddress].set({
+            key: guardian1,
+            value: GuardianStorage(GuardianStatus.REQUESTED, guardianWeights[1])
+        });
+    }
 }
