b1a202bc7a470ae7e651854d90cde7ad5033341b
JohnGuilding <jwguilding@gmail.com>
2024-07-07 14:07:06 +0200

    Fix deployment bug

--- script/DeploySafeNativeRecovery.s.sol
+++ script/DeploySafeNativeRecovery.s.sol
@@ -14,7 +14,7 @@ contract DeploySafeNativeRecovery_Script is Script {
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
         address verifier = vm.envOr("VERIFIER", address(0));
         address dkimRegistry = vm.envOr("DKIM_REGISTRY", address(0));
-        address dkimRegistrySigner = vm.envOr("SIGNER", address(0));
+        address dkimRegistrySigner = vm.envOr("DKIM_SIGNER", address(0));
         address emailAuthImpl = vm.envOr("EMAIL_AUTH_IMPL", address(0));
 
         if (verifier == address(0)) {
@@ -33,7 +33,7 @@ contract DeploySafeNativeRecovery_Script is Script {
             console.log("Deployed Email Auth at", emailAuthImpl);
         }
 
-        address module = address(new SafeEmailRecoveryModule(verifier, emailAuthImpl, dkimRegistry));
+        address module = address(new SafeEmailRecoveryModule(verifier, dkimRegistry, emailAuthImpl));
 
         console.log("Deployed Email Recovery Module at  ", vm.toString(module));
 
