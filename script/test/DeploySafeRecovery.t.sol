10cc28539f172d35b9b6a6e5b4943230c1c174c5
wshino <wshinohara@gmail.com>
2024-09-22 12:58:51 +0900

    Add tests for scripts

---
+++ script/test/DeploySafeRecovery.t.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+
+import "forge-std/Test.sol";
+import "forge-std/console2.sol";
+import { DeploySafeRecovery_Script } from "../DeploySafeRecovery.s.sol";
+import { BaseDeployTest } from "./BaseDeployTest.sol";
+
+contract DeploySafeRecovery_Test is BaseDeployTest {
+    /**
+     * @notice Tests the standard run scenario.
+     */
+    function test_run() public {
+        BaseDeployTest.setUp();
+        DeploySafeRecovery_Script target = new DeploySafeRecovery_Script();
+        target.run();
+    }
+
+    /**
+     * @notice Tests the run function without a verifier set.
+     */
+    function test_run_no_verifier() public {
+        BaseDeployTest.setUp();
+        vm.setEnv("VERIFIER", vm.toString(address(0)));
+        DeploySafeRecovery_Script target = new DeploySafeRecovery_Script();
+        target.run();
+    }
+
+    /**
+     * @notice Tests the run function without a DKIM registry.
+     */
+    function test_run_no_dkim_registry() public {
+        BaseDeployTest.setUp();
+        vm.setEnv("DKIM_REGISTRY", vm.toString(address(0)));
+        DeploySafeRecovery_Script target = new DeploySafeRecovery_Script();
+        target.run();
+    }
+}
