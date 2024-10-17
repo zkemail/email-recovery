6b1cc378d9b641fb5acf1dc0025dd5363bf15c0e
wshino <wshinohara@gmail.com>
2024-10-17 12:52:57 +0900

    Add missing low risk functions

--- test/unit/modules/UniversalEmailRecoveryModule/isModuleType.t.sol
+++ test/unit/modules/UniversalEmailRecoveryModule/isModuleType.t.sol
@@ -1,12 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
+import { ModuleKitHelpers } from "modulekit/ModuleKit.sol";
 import { UnitBase } from "../../UnitBase.t.sol";
+import { MODULE_TYPE_EXECUTOR, MODULE_TYPE_VALIDATOR } from "modulekit/external/ERC7579.sol";
+import { UniversalEmailRecoveryModule } from "src/modules/UniversalEmailRecoveryModule.sol";
 
 contract UniversalEmailRecoveryModule_isModuleType_Test is UnitBase {
     function setUp() public override {
         super.setUp();
     }
 
-    function test_IsModuleType_ReturnsModuleType() public view { }
+    function test_IsModuleType_ReturnsModuleType() public {
+        // Verify that the module type is correct
+        bool isExecutor = emailRecoveryModule.isModuleType(MODULE_TYPE_EXECUTOR);
+        assertTrue(isExecutor, "Should be an executor module");
+
+        bool isValidator = emailRecoveryModule.isModuleType(MODULE_TYPE_VALIDATOR);
+        assertFalse(isValidator, "Should not be a validator module");
+    }
 }
