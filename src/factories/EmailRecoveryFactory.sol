41b2565e9deebbe451d43a322b2bdeac97d6fa38
masaun <masaun@protonmail.com>
2025-01-09 12:46:05 +0900

    [SC]: Add the address of 'emailAuthImpl' and 'eoaVerifier' to the constructor() and deployEmailRecoveryModule() in the EmailRecoveryFactory.sol

--- src/factories/EmailRecoveryFactory.sol
+++ src/factories/EmailRecoveryFactory.sol
@@ -18,9 +18,19 @@ contract EmailRecoveryFactory {
     address public immutable verifier;
 
     /**
+     * @notice Address of the EOA verifier used by the recovery module.
+     */
+    address public immutable eoaVerifier; /// @dev - This is originally come from the EOA-TX-builder module.
+
+    /**
      * @notice Address of the EmailAuth.sol implementation.
      */
     address public immutable emailAuthImpl;
+
+    /**
+     * @notice Address of the EoaAuth.sol implementation.
+     */
+    address public immutable eoaAuthImpl; /// @dev - This is originally come from the EOA-TX-builder module.
 
     event EmailRecoveryModuleDeployed(
         address emailRecoveryModule,
@@ -32,7 +42,7 @@ contract EmailRecoveryFactory {
     error InvalidVerifier();
     error InvalidEmailAuthImpl();
 
-    constructor(address _verifier, address _emailAuthImpl) {
+    constructor(address _verifier, address _emailAuthImpl, address _eoaVerifier, address _eoaAuthImpl) {
         if (_verifier == address(0)) {
             revert InvalidVerifier();
         }
@@ -40,7 +50,9 @@ contract EmailRecoveryFactory {
             revert InvalidEmailAuthImpl();
         }
         verifier = _verifier;
+        eoaVerifier = _eoaVerifier;     /// @dev - This is originally come from the EOA-TX-builder module.
         emailAuthImpl = _emailAuthImpl;
+        eoaAuthImpl = _eoaAuthImpl;     /// @dev - This is originally come from the EOA-TX-builder module.
     }
 
     /**
@@ -84,6 +96,7 @@ contract EmailRecoveryFactory {
         address emailRecoveryModule = address(
             new EmailRecoveryModule{ salt: recoveryModuleSalt }(
                 verifier,
+                eoaVerifier, /// @dev - EOA Verifier, which is originally come from the EOA-TX-builder module.
                 dkimRegistry,
                 emailAuthImpl,
                 commandHandler,
