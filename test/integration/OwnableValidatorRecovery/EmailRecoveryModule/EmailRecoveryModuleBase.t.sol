fbc3ca8b04398c370f7b3adb5157d3e535e1d563
aalimsahin <alimsahin0007@gmail.com>
2025-02-07 15:45:30 +0300

    feat: added new timestamp test

--- test/integration/OwnableValidatorRecovery/EmailRecoveryModule/EmailRecoveryModuleBase.t.sol
+++ test/integration/OwnableValidatorRecovery/EmailRecoveryModule/EmailRecoveryModuleBase.t.sol
@@ -148,6 +148,10 @@ abstract contract OwnableValidatorRecovery_EmailRecoveryModule_Base is BaseTest 
         // // 5- verify that only the killSwitchAuthorizer can set the transaction initiator
         // emailRecoveryModule.setTransactionInitiator(address(this), true);
 
+        // // 6- confirm that it does not work before 6 months when the transaction initiator flag is not true
+        // // (Expect 15 tests to fail under this condition.)
+        // vm.warp(block.timestamp + 7_884_000); // 3 months
+
         if (getCommandHandlerType() == CommandHandlerType.AccountHidingRecoveryCommandHandler) {
             AccountHidingRecoveryCommandHandler(commandHandlerAddress).storeAccountHash(
                 accountAddress1
