63f71ced1f4e5f08ba9e9c385c19f558e65d15f7
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2025-03-22 19:18:38 +0530

    feat: JwtGuardianVerifier added + test setup

---
+++ src/JwtGuardianVerifier.sol
@@ -0,0 +1,258 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import {IDKIMRegistry} from "@zk-email/contracts/DKIMRegistry.sol";
+
+import {IGuardianVerifier} from "./interfaces/IGuardianVerifier.sol";
+import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+import {IVerifier, EmailProof} from "./interfaces/IJwtVerifier.sol";
+
+/**
+ * @title JwtGuardianVerifier
+ * @notice Provides a mechanism for guardian verification using jwt token proofs
+ * @dev The underlying IGuardianVerifier provides the interface for proof verification.
+ */
+contract JwtGuardianVerifier is IGuardianVerifier, Initializable {
+    // NOTE: Temporary bypass, remove after the upgrade
+    address _owner;
+
+    bool public timestampCheckEnabled;
+    uint256 public lastTimestamp;
+
+    mapping(bytes32 emailNullifier => bool used) public usedNullifiers;
+
+    IDKIMRegistry public jwtRegistry;
+
+    IVerifier public verifier;
+
+    struct JwtData {
+        string domainName;
+        uint256 timestamp;
+        string maskedCommand;
+        bytes32 accountSalt;
+        bool isCodeExist;
+        bool isRecovery;
+    }
+
+    /// @notice Returns the address of the JWT registry contract.
+    /// @return address The address of the DKIM registry contract.
+    function jwtRegistryAddr() public view returns (address) {
+        return address(jwtRegistry);
+    }
+
+    /// @notice Returns the address of the verifier contract.
+    /// @return address The Address of the verifier contract.
+    function verifierAddr() public view returns (address) {
+        return address(verifier);
+    }
+
+    constructor() {}
+
+    //TODO: Maybe accountSalt can be passed while intializing the contract
+    /**
+     * @dev Initializes the contract with the JWT registry, verifier addresses.
+     * @notice Addresses are hardcoded for the initial implementation.
+     *
+     * @param {recoveredAccount} The address of the account to be recovered.
+     * @param {accountSalt} The salt used to derive the account address.
+     * @param {initData} The initialization data.
+     */
+    function initialize(
+        address /* recoveredAccount */,
+        bytes32 /* accountSalt */,
+        bytes calldata initData
+    ) public initializer {
+        // NOTE: Temporary bypass, remove after the upgrade
+        _owner = msg.sender;
+
+        // address _dkimRegistry = 0x3D3935B3C030893f118a84C92C66dF1B9E4169d6;
+        // address _verifier = 0x3E5f29a7cCeb30D5FCD90078430CA110c2985716;
+
+        (address _jwtRegistry, address _verifier) = abi.decode(
+            initData,
+            (address, address)
+        );
+
+        jwtRegistry = IDKIMRegistry(_jwtRegistry);
+        verifier = IVerifier(_verifier);
+
+        timestampCheckEnabled = true;
+    }
+
+    // NOTE: Temporary bypass, remove after the upgrade
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Verify the proof
+     * Recommended to use when proof verification is done on-chain or when called from another contract
+     *
+     * @notice Reverts if the proof is invalid
+     *
+     * @param recoveredAccount Account to be recovered
+     * @param proof Proof data
+     * proof.data: JwtData
+     * proof.publicInputs: [publicKeyHash, emailNullifier]
+     * proof.proof: zk-SNARK proof
+     *
+     * @return isVerified if the proof is valid
+     */
+    function verifyProofStrict(
+        address recoveredAccount,
+        ProofData memory proof
+    ) public view returns (bool) {
+        // Parse the extra data
+        JwtData memory jwtData = abi.decode(proof.data, (JwtData));
+
+        require(jwtData.isCodeExist == true, "isCodeExist is false");
+
+        EmailProof memory jwtProof = EmailProof({
+            domainName: jwtData.domainName,
+            publicKeyHash: proof.publicInputs[0],
+            timestamp: jwtData.timestamp,
+            maskedCommand: jwtData.maskedCommand,
+            emailNullifier: proof.publicInputs[1],
+            accountSalt: jwtData.accountSalt,
+            isCodeExist: jwtData.isCodeExist,
+            proof: proof.proof
+        });
+
+        // require(
+        //     jwtRegistry.isDKIMPublicKeyHashValid(
+        //         jwtProof.domainName,
+        //         jwtProof.publicKeyHash
+        //     ) == true,
+        //     "invalid dkim public key hash"
+        // );
+
+        require(
+            usedNullifiers[jwtProof.emailNullifier] == false,
+            "email nullifier already used"
+        );
+
+        // TODO: match with the accountSalt added during initialization
+        // require(
+        //     accountSalt == jwtProof.accountSalt,
+        //     "invalid account salt"
+        // );
+
+        require(
+            timestampCheckEnabled == false ||
+                jwtProof.timestamp == 0 ||
+                jwtProof.timestamp > lastTimestamp,
+            "invalid timestamp"
+        );
+
+        require(
+            bytes(jwtProof.maskedCommand).length <= verifier.getCommandBytes(),
+            "invalid masked command length"
+        );
+
+        require(
+            verifier.verifyEmailProof(jwtProof) == true,
+            "invalid email proof"
+        );
+
+        // TODO: How to handle the nullifier update
+        // usedNullifiers[jwtProof.emailNullifier] = true;
+        // if (timestampCheckEnabled && jwtProof.timestamp != 0) {
+        //     lastTimestamp = jwtProof.timestamp;
+        // }
+
+        return (true);
+    }
+
+    /**
+     * @dev Verify the proof and return the result and error message
+     * Recommended to use when proof verification is done off-chain
+     *
+     * @notice Can be used to check the proof and get the error message
+     * @notice No revert if the proof is invalid
+     *
+     * @param recoveredAccount Account to be recovered
+     * @param proof Proof data
+     * proof.data: JwtData
+     * proof.publicInputs: [publicKeyHash, emailNullifier]
+     * proof.proof: zk-SNARK proof
+     *
+     * @return isVerified if the proof is valid
+     * @return error message if the proof is invalid
+     */
+    // Lint Error: Cyclomatic complexity ( too many if else statements )
+    function verifyProof(
+        address recoveredAccount,
+        ProofData memory proof
+    ) public view returns (bool, string memory) {
+        JwtData memory jwtData = abi.decode(proof.data, (JwtData));
+
+        if (jwtData.isCodeExist == false) {
+            return (false, "isCodeExist is false");
+        }
+
+        EmailProof memory jwtProof = EmailProof({
+            domainName: jwtData.domainName,
+            publicKeyHash: proof.publicInputs[0],
+            timestamp: jwtData.timestamp,
+            maskedCommand: jwtData.maskedCommand,
+            emailNullifier: proof.publicInputs[1],
+            accountSalt: jwtData.accountSalt,
+            isCodeExist: jwtData.isCodeExist,
+            proof: proof.proof
+        });
+
+        if (
+            jwtRegistry.isDKIMPublicKeyHashValid(
+                jwtProof.domainName,
+                jwtProof.publicKeyHash
+            ) == false
+        ) {
+            return (false, "invalid dkim public key hash");
+        }
+        // require(
+        //     jwtRegistry.isDKIMPublicKeyHashValid(
+        //         jwtProof.domainName,
+        //         jwtProof.publicKeyHash
+        //     ) == true,
+        //     "invalid dkim public key hash"
+        // );
+
+        if (usedNullifiers[jwtProof.emailNullifier] == true) {
+            return (false, "email nullifier already used");
+        }
+
+        // TODO: match with the accountSalt added during initialization
+        // if (emailProof.accountSalt != emailData.accountSalt) {
+        //     return (false, "invalid account salt");
+        // }
+
+        if (
+            timestampCheckEnabled == true && jwtProof.timestamp < lastTimestamp
+        ) {
+            return (false, "invalid timestamp");
+        }
+
+        if (bytes(jwtProof.maskedCommand).length > verifier.getCommandBytes()) {
+            return (false, "invalid masked command length");
+        }
+
+        if (verifier.verifyEmailProof(jwtProof) == false) {
+            return (false, "invalid email proof");
+        }
+
+        // TODO: How to handle the nullifier update
+        // usedNullifiers[jwtProof.emailNullifier] = true;
+        // if (timestampCheckEnabled && jwtProof.timestamp != 0) {
+        //     lastTimestamp = jwtProof.timestamp;
+        // }
+
+        return (true, "");
+    }
+
+    /// @notice Enables or disables the timestamp check.
+    /// @dev This function can only be called by the controller.
+    /// @param _enabled Boolean flag to enable or disable the timestamp check.
+    function setTimestampCheckEnabled(bool _enabled) public {
+        timestampCheckEnabled = _enabled;
+    }
+}
---
+++ test-new/JwtGuardianVerifier/JwtGuardianVerifierBase.t.sol
@@ -0,0 +1,367 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import {ModuleKitHelpers} from "modulekit/ModuleKit.sol";
+import {MODULE_TYPE_EXECUTOR, MODULE_TYPE_VALIDATOR} from "modulekit/accounts/common/interfaces/IERC7579Module.sol";
+
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+import {EmailRecoveryFactory} from "src/factories/EmailRecoveryFactory.sol";
+import {EmailRecoveryModule} from "src/modules/EmailRecoveryModule.sol";
+
+import {BaseTest, CommandHandlerType} from "../Base.t.sol";
+
+import {EmailProof} from "@zk-email/ether-email-auth-contracts/src/interfaces/IVerifier.sol";
+
+import {MockJwtVerifier} from "src/test/MockJwtVerifier.sol";
+import {JwtGuardianVerifier} from "src/JwtGuardianVerifier.sol";
+import {IGuardianVerifier} from "src/interfaces/IGuardianVerifier.sol";
+
+import {UserOverrideableDKIMRegistry} from "@zk-email/contracts/UserOverrideableDKIMRegistry.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+
+interface IEmailRecoveryModule {
+    function handleAcceptance(
+        address guardianVerifierImplementation,
+        address account,
+        bytes32 accountSalt,
+        bytes memory verifierInitData,
+        IGuardianVerifier.ProofData memory proofData
+    ) external;
+
+    function handleRecovery(
+        address guardian,
+        address account,
+        bytes32 accountSalt,
+        bytes32 recoveryDataHash,
+        IGuardianVerifier.ProofData memory proofData
+    ) external;
+
+    function completeRecovery(
+        address account,
+        bytes memory completeCalldata
+    ) external;
+}
+
+/**
+ * Base setup for Jwt Guardian verifier
+ */
+abstract contract OwnableValidatorRecovery_AbstractedRecoveryModule_Base is
+    BaseTest
+{
+    UserOverrideableDKIMRegistry public dkimRegistry;
+    MockGroth16Verifier public verifier;
+
+    using ModuleKitHelpers for *;
+    using Strings for uint256;
+    using Strings for address;
+
+    EmailRecoveryFactory public emailRecoveryFactory;
+    EmailRecoveryModule public emailRecoveryModule;
+
+    address public emailRecoveryModuleAddress;
+
+    bytes public recoveryData1;
+    bytes public recoveryData2;
+    bytes public recoveryData3;
+    bytes32 public recoveryDataHash1;
+    bytes32 public recoveryDataHash2;
+    bytes32 public recoveryDataHash3;
+
+    address public jwtGuardianVerifierImplementation;
+    bytes public jwtGuardianVerifierInitData;
+
+    function setUp() public virtual override {
+        super.setUp();
+
+        vm.startPrank(zkEmailDeployer);
+
+        uint256 setTimeDelay = 0;
+        UserOverrideableDKIMRegistry overrideableDkimImpl = new UserOverrideableDKIMRegistry();
+        ERC1967Proxy dkimProxy = new ERC1967Proxy(
+            address(overrideableDkimImpl),
+            abi.encodeCall(
+                overrideableDkimImpl.initialize,
+                (zkEmailDeployer, zkEmailDeployer, setTimeDelay)
+            )
+        );
+        dkimRegistry = UserOverrideableDKIMRegistry(address(dkimProxy));
+
+        dkimRegistry.setDKIMPublicKeyHash(
+            domainName,
+            publicKeyHash,
+            zkEmailDeployer,
+            new bytes(0)
+        );
+
+        verifier = new MockJwtVerifier();
+
+        vm.stopPrank();
+
+        // Setup for the email guardian verifier
+        bytes memory initData = abi.encode(
+            address(dkimRegistry),
+            address(verifier)
+        );
+        jwtGuardianVerifierInitData = initData;
+
+        // Deploy the email guardian verifier
+        jwtGuardianVerifierImplementation = address(new JwtGuardianVerifier());
+
+        guardians1 = new address[](3);
+        guardians1[0] = computeGuardianVerifierAuthAddress(
+            jwtGuardianVerifierImplementation,
+            instance1.account,
+            accountSalt1,
+            jwtGuardianVerifierInitData
+        );
+        guardians1[1] = computeGuardianVerifierAuthAddress(
+            jwtGuardianVerifierImplementation,
+            instance1.account,
+            accountSalt2,
+            jwtGuardianVerifierInitData
+        );
+        guardians1[2] = computeGuardianVerifierAuthAddress(
+            jwtGuardianVerifierImplementation,
+            instance1.account,
+            accountSalt3,
+            jwtGuardianVerifierInitData
+        );
+
+        // INITIAL SETUP
+        bytes memory changeOwnerCalldata1 = abi.encodeWithSelector(
+            functionSelector,
+            newOwner1
+        );
+        bytes memory changeOwnerCalldata2 = abi.encodeWithSelector(
+            functionSelector,
+            newOwner2
+        );
+        bytes memory changeOwnerCalldata3 = abi.encodeWithSelector(
+            functionSelector,
+            newOwner3
+        );
+        recoveryData1 = abi.encode(validatorAddress, changeOwnerCalldata1);
+        recoveryData2 = abi.encode(validatorAddress, changeOwnerCalldata2);
+        recoveryData3 = abi.encode(validatorAddress, changeOwnerCalldata3);
+        recoveryDataHash1 = keccak256(recoveryData1);
+        recoveryDataHash2 = keccak256(recoveryData2);
+        recoveryDataHash3 = keccak256(recoveryData3);
+
+        bytes memory recoveryModuleInstallData1 = abi.encode(
+            isInstalledContext,
+            guardians1,
+            guardianWeights,
+            threshold,
+            delay,
+            expiry
+        );
+
+        // Install modules for account 1
+        instance1.installModule({
+            moduleTypeId: MODULE_TYPE_VALIDATOR,
+            module: validatorAddress,
+            data: abi.encode(owner1)
+        });
+        instance1.installModule({
+            moduleTypeId: MODULE_TYPE_EXECUTOR,
+            module: emailRecoveryModuleAddress,
+            data: recoveryModuleInstallData1
+        });
+    }
+
+    // Helper functions
+    function computeGuardianVerifierAuthAddress(
+        address guardianVerifierImplementation,
+        address account,
+        bytes32 accountSalt,
+        bytes memory verifierInitData
+    ) public view override returns (address) {
+        return
+            emailRecoveryModule.computeGuardianVerifierAddress(
+                guardianVerifierImplementation,
+                account,
+                accountSalt,
+                verifierInitData
+            );
+    }
+
+    // Helper functions
+    function deployModule() public override {
+        // Deploy the email recovery factory after removing email guardian verifier related logic
+        emailRecoveryFactory = new EmailRecoveryFactory();
+
+        bytes32 recoveryModuleSalt = bytes32(uint256(0));
+        emailRecoveryModuleAddress = emailRecoveryFactory
+            .deployEmailRecoveryModule(
+                recoveryModuleSalt,
+                minimumDelay,
+                killSwitchAuthorizer,
+                validatorAddress,
+                functionSelector
+            );
+        emailRecoveryModule = EmailRecoveryModule(emailRecoveryModuleAddress);
+
+        if (
+            getCommandHandlerType() ==
+            CommandHandlerType.AccountHidingRecoveryCommandHandler
+        ) {
+            AccountHidingRecoveryCommandHandler(commandHandlerAddress)
+                .storeAccountHash(accountAddress1);
+            AccountHidingRecoveryCommandHandler(commandHandlerAddress)
+                .storeAccountHash(accountAddress2);
+            AccountHidingRecoveryCommandHandler(commandHandlerAddress)
+                .storeAccountHash(accountAddress3);
+        }
+    }
+
+    // Helper functions
+    function setRecoveryData() public override {
+        functionSelector = bytes4(keccak256(bytes("changeOwner(address)")));
+        recoveryCalldata = abi.encodeWithSelector(functionSelector, newOwner1);
+        recoveryData = abi.encode(validatorAddress, recoveryCalldata);
+        recoveryDataHash = keccak256(recoveryData);
+    }
+
+    // TODO: Implement the following function
+    function generateMockJwtProof(
+        string memory command,
+        bytes32 nullifier,
+        bytes32 accountSalt
+    ) public view returns (EmailProof memory) {
+        EmailProof memory emailProof;
+        emailProof.domainName = "gmail.com";
+        emailProof.publicKeyHash = bytes32(
+            vm.parseUint(
+                "6632353713085157925504008443078919716322386156160602218536961028046468237192"
+            )
+        );
+        emailProof.timestamp = block.timestamp;
+        emailProof.maskedCommand = command;
+        emailProof.emailNullifier = nullifier;
+        emailProof.accountSalt = accountSalt;
+        emailProof.isCodeExist = true;
+        emailProof.proof = bytes("0");
+
+        return emailProof;
+    }
+
+    function acceptGuardian(
+        address guardianVerifierImplementation,
+        address account,
+        address guardian,
+        address emailRecoveryModule,
+        bytes32 accountSalt,
+        bytes memory verifierInitData
+    ) public {
+        EmailGuardianVerifier.ProofData
+            memory proofData = getAcceptanceJwtProofData(
+                account,
+                guardian,
+                emailRecoveryModule,
+                accountSalt
+            );
+        IEmailRecoveryModule(emailRecoveryModule).handleAcceptance(
+            guardianVerifierImplementation,
+            account,
+            accountSalt,
+            verifierInitData,
+            proofData
+        );
+    }
+
+    function getAcceptanceJwtProofData(
+        address account,
+        address guardian,
+        address emailRecoveryModule,
+        bytes32 accountSalt
+    ) public returns (IGuardianVerifier.ProofData memory proofData) {
+        bytes32 nullifier = generateNewNullifier();
+
+        EmailProof memory jwtProof = generateMockJwtProof(
+            command,
+            nullifier,
+            accountSalt
+        );
+
+        JwtGuardianVerifier.JwtData memory jwtData = EmailGuardianVerifier
+            .JwtData({
+                domainName: jwtProof.domainName,
+                timestamp: jwtProof.timestamp,
+                maskedCommand: jwtProof.maskedCommand,
+                accountSalt: accountSalt,
+                isCodeExist: jwtProof.isCodeExist,
+                isRecovery: false // Acceptance
+            });
+
+        bytes32[] memory acceptancePublicInputs = new bytes32[](2);
+        acceptancePublicInputs[0] = jwtProof.publicKeyHash; // publicKeyHash
+        acceptancePublicInputs[1] = jwtProof.emailNullifier; // emailNullifier
+
+        proofData = IGuardianVerifier.ProofData({
+            proof: emailProof.proof,
+            publicInputs: acceptancePublicInputs,
+            data: abi.encode(jwtData)
+        });
+    }
+
+    function handleRecovery(
+        address account,
+        address guardian,
+        bytes32 _recoveryDataHash,
+        address emailRecoveryModule,
+        bytes32 accountSalt
+    ) public {
+        EmailGuardianVerifier.ProofData
+            memory proofData = getRecoveryJwtProofData(
+                account,
+                guardian,
+                _recoveryDataHash,
+                emailRecoveryModule,
+                accountSalt
+            );
+        IEmailRecoveryModule(emailRecoveryModule).handleRecovery(
+            guardian,
+            account,
+            accountSalt,
+            _recoveryDataHash,
+            proofData
+        );
+    }
+
+    // WithAccountSalt variation - used for creating incorrect recovery setups
+    function getRecoveryJwtProofData(
+        address account,
+        address guardian,
+        bytes32 _recoveryDataHash,
+        address emailRecoveryModule,
+        bytes32 accountSalt
+    ) public returns (IGuardianVerifier.ProofData memory proofData) {
+        bytes32 nullifier = generateNewNullifier();
+
+        EmailProof memory jwtProof = generateMockJwtProof(
+            command,
+            nullifier,
+            accountSalt
+        );
+
+        JwtGuardianVerifier.JwtData memory jwtData = EmailGuardianVerifier
+            .JwtData({
+                domainName: jwtProof.domainName,
+                timestamp: jwtProof.timestamp,
+                maskedCommand: jwtProof.maskedCommand,
+                accountSalt: accountSalt,
+                isCodeExist: jwtProof.isCodeExist,
+                isRecovery: false // Acceptance
+            });
+
+        bytes32[] memory acceptancePublicInputs = new bytes32[](2);
+        acceptancePublicInputs[0] = emailProof.publicKeyHash; // publicKeyHash
+        acceptancePublicInputs[1] = emailProof.emailNullifier; // emailNullifier
+
+        proofData = IGuardianVerifier.ProofData({
+            proof: emailProof.proof,
+            publicInputs: acceptancePublicInputs,
+            data: abi.encode(jwtData)
+        });
+    }
+}
